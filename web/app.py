e321c3877449a73241fb22112eae5374c8d2e1a6
Dharmesh-Poddar <dharmeshpoddar3@gmail.com>
2020-04-06 10:04:27 +0530

    folder structure

---
+++ web/app.py
@@ -0,0 +1,51 @@
+from flask import Flask,jsonify,request
+from flask_restful import Api,Resource
+
+app=Flask(__name__)
+api=Api(app)
+
+client= MongoClient("mongodb://db:27017")
+db= client.SimilarityDB
+users= db["Users"]
+
+def UserExist(username):
+	if users.find({"Username":username}).count()==0:
+		return False
+	else:
+	    return True
+	    
+
+
+class Register(Resource):
+	def post(self):
+		postedData =request.get_json()
+		username= postedData["username"]
+		password= postedData["password"]
+
+		if UserExist(Username):
+			retJson ={
+			    "status": 301,
+			    "msg": "invalid username"
+			}
+			return jsonify(retJson)
+
+		hashed_pw= bcrypt.hashpw(password.encode('utf8'),bcrypt.gensalt())	
+
+		users.insert({
+			"Username":username,
+			"Password": hashed_pw,
+			"Tokens": 6 
+		})
+        
+        retJson={
+            "status":200,
+            "msg": "You have successfully signed up to the api"
+        }
+        return jsonify(retJson)
+
+
+
+
+
+if __name__=='__main__':
+	app.run('debug='True')
